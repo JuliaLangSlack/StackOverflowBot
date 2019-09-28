9fbb7bc4c7b3a7b60a77c828da8e868e779fe3e2
Logan Kilpatrick <lok026@g.harvard.edu>
2019-09-28 19:28:48 -0400

    Update StackOverflow.py
    Added a more robust message format! It's experimental but should work based on my tests.

--- .github/workflows/StackOverflow.py
+++ .github/workflows/StackOverflow.py
@@ -21,15 +21,31 @@ for k,v in posts.items():
     if k == "items":
         #print("KEY: " + str(k) + " VALUES: " + str(v))
         for item in v: 
+            title = ""
+            owner = ""
+            is_answered = ""
+            link = ""            
             for k,v in item.items():
-                print(k)
-                if k == "link":
-                    print(v)
-                    data = ('{"text":"%s"}' % v)
+                
+                if k == "title":
+                    title = v
+                elif k == "owner":
+                    for name_key, name_value in v.items():
+                        if name_key == "display_name":
+                            owner = name_value
+                    #print("Owner: " + str(owner)) #Used for DEBUG
+                elif k == "is_answered":
+                    print("is_answered" + str(v))
+                    is_answered = v
+                elif k == "link":
+                    link = v
+                    currenttime = int(time.time())
+                    data = ('{ "attachments": [ { "color": "#36a64f", "author_name": "author_name : %s", "title": "%s", "title_link": "%s", "text": "%s", "footer": "Slack API", "ts": %d, "fields": [ { "title": "Is Answered", "value": "%s", "short": false } ] } ] }' % (owner, title, link, link, currenttime, is_answered))
                     
                     response = requests.post(webHookURL, headers=headers, data=data)
                     
                     print(response)
+                    
                 #End of if
             #End of for
         #End of internal for loop. 
