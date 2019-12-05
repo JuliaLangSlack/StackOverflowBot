90f63994727cbb6ec27ec89b25eeb054aa7e2d47
Kim Fung <fungtszto@gmail.com>
2019-12-05 17:34:58 +0000

    Add files via upload

---
+++ .github/workflows/RePostStackOverflow.jl
@@ -0,0 +1,28 @@
+using StackOverflow, Dates, HTTP, Slack
+
+#webHookURL = ARGS[2]
+endpoint = "/services/TQVJBU534/BR8C1LMPS/42thawJz34SWSgZCpniyLBSE"
+yesterday = trunc(Int64,floor(Dates.time()))- 86400
+eightDaysAgo = (yesterday - 604800)
+posts = getrecentquestionsfortag(fromdate = string(eightDaysAgo), todate = string(yesterday))
+
+for question in posts
+    link = question.link[1]
+    view_count = question.view_count[1]
+    owner = question.owner[1]
+    score = question.score[1]
+    title = question.title[1]
+    answer_count = question.answer_count[1]
+    if (answer_count == 0)
+        data = Dict("attachments" => [Dict("color" => "#36a64f",
+                                            "title" => "$(string(title))",
+                                            "title_link" => "$(link)",
+                                            "text" => "$(link)",
+                                            "footer" => "Slack API",
+                                            "ts" => "$(string(currenttime))",
+                                            "fields" => [Dict("title" => "Answer Count: $(answer_count)", "short" => false)]
+                                        )])
+        response = sendattachmenttoslack(data, endpoint)
+        println(response)
+    end
+end
---
+++ .github/workflows/StackOverflow.jl
@@ -0,0 +1,27 @@
+using StackOverflow, Dates, HTTP, Slack
+
+#webHookURL = ARGS[2]
+endpoint = "/services/TQVJBU534/BR8C1LMPS/42thawJz34SWSgZCpniyLBSE"
+currenttime = trunc(Int64,floor(Dates.time())) #Get current time in Unix Epcot Time
+thirtyMinsAgo = currenttime - 1800 #rewind 30 minutes since that's the frequency the job run's at.
+posts = getrecentquestionsfortag(fromdate = string(thirtyMinsAgo), todate = string(currenttime))
+
+for question in posts
+    link = question.link[1]
+    view_count = question.view_count[1]
+    owner = question.owner[1]
+    score = question.score[1]
+    title = question.title[1]
+    answer_count = question.answer_count[1]
+    #data = ("[ { \"color\": \"#36a64f\", \"title\": \"$(string(title))\", \"title_link\": \"$(link)\", \"text\": \"$(link)\", \"footer\": \"Slack API\", \"ts\": \"$(string(currenttime))\", \"fields\": [ { \"title\": \"Answer Count: $(answer_count)\", \"short\": false } ] } ] ")
+    data = Dict("attachments" => [Dict("color" => "#36a64f",
+                                        "title" => "$(string(title))",
+                                        "title_link" => "$(link)",
+                                        "text" => "$(link)",
+                                        "footer" => "Slack API",
+                                        "ts" => "$(string(currenttime))",
+                                        "fields" => [Dict("title" => "Answer Count: $(answer_count)", "short" => false)]
+                                    )])
+    response = sendattachmenttoslack(data, endpoint)
+    println(response)
+end
