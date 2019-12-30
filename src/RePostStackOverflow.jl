e7658fd2e1245dc371d6814f1944428828f778dc
Logan Kilpatrick <lok026@g.harvard.edu>
2019-12-30 11:29:28 -0600

    Update RePostStackOverflow.jl
    Fixed color fo re-posts and added the disclaimer at the top of the re-post queue of questions.

--- src/RePostStackOverflow.jl
+++ src/RePostStackOverflow.jl
@@ -5,8 +5,16 @@ yesterday = trunc(Int64,floor(Dates.time()))- 86400
 currenttime = Dates.time()
 eightDaysAgo = (yesterday - 604800)
 posts = getrecentquestionsfortag(fromdate = string(eightDaysAgo), todate = string(yesterday))
+flag_holder = false
 
 for question in posts
+    
+    if flag_holder == false
+        response = sendattachmenttoslack("The following questions are being re-posted since no one answered yet:", endpoint)
+        println(response)
+        flag_holder = true
+    end
+    
     link = question.link[1]
     view_count = question.view_count[1]
     owner = question.owner[1]
@@ -14,12 +22,12 @@ for question in posts
     title = question.title[1]
     answer_count = question.answer_count[1]
     if (answer_count == 0)
-        data = Dict("attachments" => [Dict("color" => "#36a64f",
+        data = Dict("attachments" => [Dict("color" => "#fd1919",
                                             "title" => "$(string(title))",
                                             "title_link" => "$(link)",
                                             "text" => "$(link)",
                                             "footer" => "Slack API",
-                                            "ts" => "$(string(currenttime))",
+                                            "ts" => "$(trunc(Int64,floor(Dates.time())))",
                                             "fields" => [Dict("title" => "Answer Count: $(answer_count)", "short" => false)]
                                         )])
         response = sendattachmenttoslack(data, endpoint)
