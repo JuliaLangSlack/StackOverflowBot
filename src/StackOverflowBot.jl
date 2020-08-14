1f6005f960cfc66b302c15221fee4fe527532a4a
Logan Kilpatrick <lok026@g.harvard.edu>
2020-08-14 09:11:57 -0500

    Update StackOverflowBot.jl
    Add an example of what an endpoint looks like.

--- src/StackOverflowBot.jl
+++ src/StackOverflowBot.jl
@@ -1,6 +1,6 @@
 using StackOverflow, Dates, HTTP, Slack
 
-endpoint = ARGS[1]
+endpoint = ARGS[1]  # Where endpoint looks like: "/services/TQVJBU534/BR8C1LMPS/42thawJz34SWSgZCpniyLBSE" (just an example). 
 
 currenttime = trunc(Int64,floor(Dates.time())) #Get current time in Unix Epcot Time
 thirtyMinsAgo = currenttime - 1800 #rewind 30 minutes since that's the frequency the job run's at.
