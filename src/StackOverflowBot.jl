51b37c2ae9bd1d6b2878ca60ad6cb1d30e0d6790
Logan Kilpatrick <23kilpatrick23@gmail.com>
2021-08-29 10:19:45 -0700

    Add comments on endpoint

--- src/StackOverflowBot.jl
+++ src/StackOverflowBot.jl
@@ -1,6 +1,8 @@
 using StackOverflow, Dates, HTTP, Slack
 
 endpoint = ARGS[1]  # Where endpoint looks like: "/services/TQVJBU534/BR8C1LMPS/42thawJz34SWSgZCpniyLBSE" (just an example). 
+# The bot uses Endpoint URL's to send messages. Find our more here: https://api.slack.com/apps/AN12MEVDH/incoming-webhooks?success=1
+# A new endpoint needs to generated for each channel you want to send posts too.
 
 currenttime = trunc(Int64,floor(Dates.time())) #Get current time in Unix Epcot Time
 thirtyMinsAgo = currenttime - 1800 #rewind 30 minutes since that's the frequency the job run's at.
