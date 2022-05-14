dbadfbe7fc152ebddef86d7770c59a48d9bb6579
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-05-14 10:18:32 -0700

    Make sure the right file is being called

--- src/TwitterBot.jl
+++ src/TwitterBot.jl
@@ -1,17 +1,13 @@
 using StackOverflow, Dates, HTTP, Slack
 
-endpoint = ARGS[1]  # Where endpoint looks like: "/services/TQVJBU534/BR8C1LMPS/42thawJz34SWSgZCpniyLBSE" (just an example). 
-# The bot uses Endpoint URL's to send messages. Find our more here: https://api.slack.com/apps/AN12MEVDH/incoming-webhooks?success=1
-# A new endpoint needs to generated for each channel you want to send posts too.
-
 currenttime = trunc(Int64,floor(Dates.time())) #Get current time in Unix Epcot Time
 thirtyMinsAgo = currenttime - 1800 #rewind 30 minutes since that's the frequency the job run's at.
 posts = getrecentquestionsfortag(fromdate = string(thirtyMinsAgo), todate = string(currenttime))
 
-API_Key = ARGS[2]
-API_Key_Secret = ARGS[3]
-Access_Token = ARGS[4]
-Access_Token_Secret = ARGS[5]
+API_Key = ARGS[1]
+API_Key_Secret = ARGS[2]
+Access_Token = ARGS[3]
+Access_Token_Secret = ARGS[4]
 
 twitterauth(API_Key, API_Key_Secret, Access_Token, Access_Token_Secret)
 
