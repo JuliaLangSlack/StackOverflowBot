6eef60c2550346fab5040b90649f21bb3202f17d
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-05-18 07:01:32 -0700

    Fix indexing issues

--- src/RePostStackOverflow.jl
+++ src/RePostStackOverflow.jl
@@ -17,12 +17,12 @@ for question in posts
         flag_holder = true
     end
     
-    link = question.link[1]
-    view_count = question.view_count[1]
-    owner = question.owner[1]
-    score = question.score[1]
-    title = question.title[1]
-    answer_count = question.answer_count[1]
+    link = question.link
+    view_count = question.view_count
+    owner = question.owner
+    score = question.score
+    title = question.title
+    answer_count = question.answer_count
     if (answer_count == 0)
         data = Dict("attachments" => [Dict("color" => "#fd1919",
                                             "title" => "$(string(title))",
